f62ea99c21388c374ff5bc2fda6e7c31514b74cf
brandolern <46857695+brandolern@users.noreply.github.com>
2019-03-24 17:53:55 -0600

    finished logic for when next train arrives

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC1G09__lZZHQE1hu8yfAOhhAk4dFYnnxQ",
+        authDomain: "train-scheduler-eb328.firebaseapp.com",
+        databaseURL: "https://train-scheduler-eb328.firebaseio.com",
+        projectId: "train-scheduler-eb328",
+        storageBucket: "train-scheduler-eb328.appspot.com",
+        messagingSenderId: "1092815152813"
+    };
+    firebase.initializeApp(config);
+    database = firebase.database();
 
     var trainName
     var trainDestination
-    var trainTime
+    var firstTime
     var trainFreq
     var nextArrival
     var minutesAway
-    var currentTime = moment().format('LT');
 
     $("#add-train").on("click", function (event) {
         event.preventDefault();
 
         trainName = $("#train-name").val().trim();
         trainDestination = $("#train-destination").val().trim();
-        trainTime = $("#train-time").val().trim();
+        firstTime = $("#train-time").val().trim();
         trainFreq = $("#train-frequency").val().trim();
-        nextArrival = currentTime
 
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        var remainder = diffTime % trainFreq;
+
+        minutesAway = trainFreq - remainder;
+        nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
 
     });
 });
