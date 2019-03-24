5395662768c32644b9afdaf804367d131c56777f
brandolern <46857695+brandolern@users.noreply.github.com>
2019-03-24 16:30:41 -0600

    created form for adding trains and code to pull values from form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
+    var trainName
+    var trainDestination
+    var trainTime
+    var trainFreq
+    var nextArrival
+    var minutesAway
+    var currentTime = moment().format('LT');
+
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
+
+        trainName = $("#train-name").val().trim();
+        trainDestination = $("#train-destination").val().trim();
+        trainTime = $("#train-time").val().trim();
+        trainFreq = $("#train-frequency").val().trim();
+        nextArrival = currentTime
+
+
+    });
 });
