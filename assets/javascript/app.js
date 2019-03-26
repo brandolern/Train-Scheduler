2e1a055cca0444edcfea05ab0599f6ef546fbad9
brandolern <46857695+brandolern@users.noreply.github.com>
2019-03-25 18:10:46 -0600

    Base program fininshed, need to add more functionality and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,28 +14,23 @@ $(document).ready(function () {
     database = firebase.database();
 
     //Train varibles
-    var trainName
-    var trainDestination
-    var firstTime
-    var trainFreq
-    var nextArrival
-    var minutesAway
+
 
     //On click function for submit button
     $("#add-train").on("click", function (event) {
         event.preventDefault();
 
-        trainName = $("#train-name").val().trim();
-        trainDestination = $("#train-destination").val().trim();
-        firstTime = $("#train-time").val().trim();
-        trainFreq = $("#train-frequency").val().trim();
+        var trainName = $("#train-name").val().trim();
+        var trainDestination = $("#train-destination").val().trim();
+        var firstTrain = $("#train-time").val().trim();
+        var trainFreq = $("#train-frequency").val().trim();
+        console.log(firstTrain)
 
-        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
         var remainder = diffTime % trainFreq;
-
-        nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
-        minutesAway = trainFreq - remainder;
+        var minutesAway = trainFreq - remainder;
+        var nextArrival = moment().add(minutesAway, "minutes").format('h:mm a');
 
         var newTrain = {
             name: trainName,
@@ -46,6 +41,31 @@ $(document).ready(function () {
         };
 
         database.ref().push(newTrain);
+
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#train-time").val("");
+        $("#train-frequency").val("");
+    });
+
+    database.ref().on("child_added", function (childSnapshot) {
+
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var frequency = childSnapshot.val().frequency;
+        var arrival = childSnapshot.val().arrival;
+        var minutesAway = childSnapshot.val().minutes;
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(name),
+            $("<td>").text(destination),
+            $("<td>").text(frequency),
+            $("<td>").text(arrival),
+            $("<td>").text(minutesAway)
+        );
+
+        $("#train-table > tbody").append(newRow);
+
     });
 
 
